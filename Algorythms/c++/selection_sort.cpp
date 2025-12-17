44b228de4d6401e15a3aaaac56c45b4ef89cf46d
swoggoi <zxcasdqweff14@gmail.com>
2025-12-17 08:55:52 +0300

    deleted_fixes

---
+++ Algorythms/c++/selection_sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    const int n = 10;
+    int arr[n]{4,2,67,1,55,3,88,9,0,33};
+    for(int i = 0; i < n- 1; ++i){
+        int min = i;
+        for(int j = i + 1;j< n; ++j){
+            if(arr[j]>arr[min]){
+                min= j;
+                swap(arr[i], arr[min]);
+            }
+        }
+    }
+    
+    for(auto& x: arr){
+        cout << x << " ";
+    }
+}
